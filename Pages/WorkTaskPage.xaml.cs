50d4b39e42f67e5e07be0cc8a128cb7801c3ff87
KingaGawron <64367504+KingaGawron@users.noreply.github.com>
2022-07-22 14:58:45 +0200

    Initial commit

---
+++ Pages/WorkTaskPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using ToDoList.Core.ViewModels.Pages;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WorkTaskPage.xaml
+    /// </summary>
+    public partial class WorkTaskPage : Page
+    {
+        public WorkTaskPage()
+        {
+            InitializeComponent();
+            DataContext = new WorkTaskPagesViewModel();
+        }
+    }
+}
